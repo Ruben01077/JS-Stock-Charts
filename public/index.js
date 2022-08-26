46003f497047959088aef800e7df9edb5cb6fb71
Ruben01077 <102193579+Ruben01077@users.noreply.github.com>
2022-08-25 17:55:17 -0700

    Delete index.js

--- public/index.js
+++
@@ -1,9 +0,0 @@
-async function main() {
-
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
-}
-
-main()
